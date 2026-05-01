71fc859e5457e0788744836b9f055d85af23ab4b
Ayuuu0424 <payushi9877@gmail.com>
2026-05-01 19:48:57 +0530

    refactor: clean up code and improve function calls

--- jsFunctions&Methods/script.js
+++ jsFunctions&Methods/script.js
@@ -10,8 +10,6 @@
 
 // console.log(c);
 
-
-
 // ...Function Expression...
 let abc = function () {
   console.log("Hello World");
@@ -19,6 +17,20 @@ let abc = function () {
 
 console.log(abc);
 
-
 // ...To call the function...
-abc()
+abc();
+
+//...Arrow Function...
+console.log(add(4, 6));
+
+console.log(add());
+
+const arrowfun = () => {
+  console.log("I'm arrow function");
+};
+
+arrowfun();
+
+const addarrow = (a, b) => {
+  console.log(addarrow(5, 6));
+};
