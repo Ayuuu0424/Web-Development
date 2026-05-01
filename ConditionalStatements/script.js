68f4b910ac07aa5a3c37ccc0ddd68e2a8020b2ca
Ayuuu0424 <payushi9877@gmail.com>
2026-05-01 18:56:24 +0530

    Arranged distinctly

---
+++ ConditionalStatements/script.js
@@ -0,0 +1,41 @@
+// let n = 29;
+
+// if (n < 20) {
+//   console.log("Not eligible to vote");
+// } else {
+//   console.log("Eligible to vote");
+// }
+
+// let n = 80;
+
+// if (n > 90) {
+//   console.log("A");
+// }
+// if (n > 75) {
+//   console.log("B");
+// }
+// if (n > 50) {
+//   console.log("C");
+// } else {
+//   console.log("Fail");
+// }
+
+// let usr = "abc";
+// let pass = "123";
+
+// if (usr === "abc" && pass === "1234") {
+//   console.log("Login successful");
+// } else {
+//   console.log("Login unsuccessful");
+// }
+
+let data;
+let name = "";
+data = name || "N/A"
+// if (name) {
+//   data = name;
+// } else {
+//   data = "N/A";
+// }
+console.log(data);
+
---
+++ jsFunctions&Methods/script.js
@@ -0,0 +1,20 @@
+// let a = 20,
+//   b = 30;
+
+// function sum(x, y) {
+//   let z = x + y
+//   console.log(z);
+// }
+
+// let c = sum(a, b);
+
+// console.log(c);
+
+
+
+// ...Function Expression...
+let abc = function () {
+  console.log("Hello World");
+};
+
+console.log(abc);
