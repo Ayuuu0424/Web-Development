801d4ea31a018be7270f356edf6d31b28e967423
Ayuuu0424 <payushi9877@gmail.com>
2026-05-08 17:54:53 +0530

    feat: add HTML structure and JavaScript functionality for bulb color control and background color changer

---
+++ jsEventHandling/script.js
@@ -0,0 +1,72 @@
+function Bulb1On() {
+  document.getElementById("bulb1").style.backgroundColor = "yellow";
+}
+
+function Bulb2On() {
+  document.getElementById("bulb1").style.backgroundColor = "coral";
+}
+
+function Bulb3On() {
+  document.getElementById("bulb1").style.backgroundColor = "pink";
+}
+
+function Bulb4On() {
+  document.getElementById("bulb1").style.backgroundColor = "red";
+}
+
+function Bulb1Off() {
+  document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+document.getElementById("bulbCoral").addEventListener("click", bulbCoral);
+
+document.getElementById("bulbColor").addEventListener("change",ChangeBulbColor);
+
+function ChangeBulbColor(){
+    const color = document.getElementById("bulbColor").value;
+    document.getElementById("bulb1").style.backgroundColor = color;
+}
+
+function resizeTo(){
+    window.location.reload();
+}
+
+// document
+//   .getElementById("bgColor")
+//   .addEventListener("change", changeBackgroundColor);
+
+// document
+//   .getElementById("headingColor")
+//   .addEventListener("change", changeHeadingColor);
+
+// document
+//   .getElementById("paraColor")
+//   .addEventListener("change", changeParagraphColor);
+
+// document.getElementById("resetBtn").addEventListener("click", resetColors);
+
+// function changeBackgroundColor() {
+//   const color = document.getElementById("bgColor").value;
+
+//   document.getElementById("mainBox").style.backgroundColor = color;
+// }
+
+// function changeHeadingColor() {
+//   const color = document.getElementById("headingColor").value;
+
+//   document.getElementById("heading").style.color = color;
+// }
+
+// function changeParagraphColor() {
+//   const color = document.getElementById("paraColor").value;
+
+//   document.getElementById("para").style.color = color;
+// }
+
+// function resetColors() {
+//   document.getElementById("mainBox").style.backgroundColor = "white";
+
+//   document.getElementById("heading").style.color = "black";
+
+//   document.getElementById("para").style.color = "black";
+// }
--- jsObjects/script.js
+++ jsObjects/script.js
@@ -1,84 +1,100 @@
-let Fdata = [
-  {
-    name: "Amit Sharma",
-    dob: "2002-05-14",
-    phone: "9876543210",
-    city: "Bhopal",
-    profession: "Software Developer",
-  },
-  {
-    name: "Priya Verma",
-    dob: "2001-09-21",
-    phone: "9123456780",
-    city: "Indore",
-    profession: "Graphic Designer",
-  },
-  {
-    name: "Rahul Singh",
-    dob: "2000-12-10",
-    phone: "9988776655",
-    city: "Delhi",
-    profession: "Teacher",
-  },
-  {
-    name: "Sneha Patel",
-    dob: "2003-03-18",
-    phone: "9012345678",
-    city: "Mumbai",
-    profession: "Doctor",
-  },
-  {
-    name: "Vikas Gupta",
-    dob: "1999-07-25",
-    phone: "9090909090",
-    city: "Pune",
-    profession: "Engineer",
-  },
-];
+// let Fdata = [
+//   {
+//     name: "Amit Sharma",
+//     dob: "2002-05-14",
+//     phone: "9876543210",
+//     city: "Bhopal",
+//     profession: "Software Developer",
+//   },
+//   {
+//     name: "Priya Verma",
+//     dob: "2001-09-21",
+//     phone: "9123456780",
+//     city: "Indore",
+//     profession: "Graphic Designer",
+//   },
+//   {
+//     name: "Rahul Singh",
+//     dob: "2000-12-10",
+//     phone: "9988776655",
+//     city: "Delhi",
+//     profession: "Teacher",
+//   },
+//   {
+//     name: "Sneha Patel",
+//     dob: "2003-03-18",
+//     phone: "9012345678",
+//     city: "Mumbai",
+//     profession: "Doctor",
+//   },
+//   {
+//     name: "Vikas Gupta",
+//     dob: "1999-07-25",
+//     phone: "9090909090",
+//     city: "Pune",
+//     profession: "Engineer",
+//   },
+// ];
 
-// Fdata.forEach((element) => {
-//   console.log(element);
-// });
+// // Fdata.forEach((element) => {
+// //   console.log(element);
+// // });
 
-// Fdata.map((element, idx) => {
-//   console.log(idx);
+// // Fdata.map((element, idx) => {
+// //   console.log(idx);
 
-//   console.log(element.phone);
-// })
+// //   console.log(element.phone);
+// // })
 
-console.log(Fdata);
-// Bracket Notation
-console.log(Fdata[1]["name"]);
-// Dot Notation
-console.log(Fdata[1].name);
+// console.log(Fdata);
+// // Bracket Notation
+// console.log(Fdata[1]["name"]);
+// // Dot Notation
+// console.log(Fdata[1].name);
 
-let vikas = {
-    name: "Vikas Gupta",
-    dob: "1999-07-25",
-    phone: "9090909090",
-    city: "Pune",
-    profession: "Engineer",
-  };
+// let vikas = {
+//   name: "Vikas Gupta",
+//   dob: "1999-07-25",
+//   phone: "9090909090",
+//   city: "Pune",
+//   profession: "Engineer",
+// };
 
-console.log(Object.keys(vikas));
-console.log(Object.values(vikas));
-console.log(Object.entries(vikas));
+// console.log(Object.keys(vikas));
+// console.log(Object.values(vikas));
+// console.log(Object.entries(vikas));
 
-let arr = [21, 2, 2, 42, 63, 7, 85, 96, 50, 2, 3, 68];
-// () k andar wala is compare function
-arr.sort((a, b) => a - b);
-console.log(arr);
+// let arr = [21, 2, 2, 42, 63, 7, 85, 96, 50, 2, 3, 68];
+// // () k andar wala is compare function
+// arr.sort((a, b) => a - b);
+// console.log(arr);
 
-console.log(arr.filter((val) => val > 20)); // prints all values greater than 20
-console.log(arr.find((val) => val > 20)); // prints the first value greater than 20
-console.log(arr.slice(0, 7));
+// console.log(arr.filter((val) => val > 20)); // prints all values greater than 20
+// console.log(arr.find((val) => val > 20)); // prints the first value greater than 20
+// console.log(arr.slice(0, 7));
 
-console.log(arr.find((val) => val === 2));
-console.log(arr.findIndex((val) => val === 2));
-console.log(arr.findLastIndex((val) => val === 2));
+// console.log(arr.find((val) => val === 2));
+// console.log(arr.findIndex((val) => val === 2));
+// console.log(arr.findLastIndex((val) => val === 2));
 
+
+// ...Strings...
 let abc = "This is Web-Development Class";
 console.log(abc);
 console.log(abc.split(" "));
 console.log(abc.split("-"));
 
+let str = "This is a String";
+console.log(str.charAt(5));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let a = 3.14;
+console.log(a);
+console.log(typeof a);
+let b = a.toString();
+console.log(b);
+
+
+let c = 3.14;
+console.log(c.toFixed(3));
