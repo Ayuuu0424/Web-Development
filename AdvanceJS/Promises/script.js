70708994da7173d00cdf87af41efbaf3a93cf0bb
Ayuuu0424 <payushi9877@gmail.com>
2026-05-19 20:20:48 +0530

    Promises in js

---
+++ AdvanceJS/Promises/script.js
@@ -0,0 +1,15 @@
+function getData(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data Sent", value);
+      resolve("Query Solved");
+    }, 5000);
+  });
+}
+
+// console.log(getData(1));
+
+const myPromise = getData(1); // getData is a function call
+
+console.log(myPromise);
+
