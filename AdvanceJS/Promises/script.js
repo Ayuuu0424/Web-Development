227711c969bac9f79dd5ba2fcf6615bbd192df95
Ayuuu0424 <payushi9877@gmail.com>
2026-05-21 20:29:21 +0530

    feat: updated getData function to be async

--- AdvanceJS/Promises/script.js
+++ AdvanceJS/Promises/script.js
@@ -1,15 +1,25 @@
-function getData(value) {
+async function getData(value) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("Data Sent", value);
-      resolve("Query Solved");
-    }, 5000);
+      if (value === 3) {
+        reject("Query Not Found");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 2000);
   });
 }
 
-console.log(getData(1));
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .then(() => getData(5))
+//   .catch((rej) => console.log(rej));
 
-const myPromise = getData(1); // getData is a function call
-
-console.log(myPromise);
-
+await getData(1);
+await getData(2);
+await getData(3);
+await getData(4);
+await getData(5);
