6e86cb445c6d8120c67eb92555dfd63367290545
Ayuuu0424 <payushi9877@gmail.com>
2026-06-06 19:12:05 +0530

    feat: initialize React app with Vite, add assets and SVG icons
    - Created a new Vite project with React.
    - Added SVG icons for various services (Bluesky, Discord, Documentation, GitHub, Social, X).
    - Included hero image and React logo SVG.
    - Set up main application structure in App.jsx and main.jsx.
    - Added global styles in index.css.
    - Configured Vite with a basic setup in vite.config.js.

--- FrontEnd/myFirstReactApp/src/pages/Login.jsx
+++ FrontEnd/myFirstReactApp/src/pages/Login.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
+
 function Login() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">Login</span>
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Welcom Back!!!</h1>
+          <br />
+
+          <form>
+            <label htmlFor="fullName">Username</label>
+            <input
+              type="text"
+              name="userName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="fullName">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+
+            <div>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
