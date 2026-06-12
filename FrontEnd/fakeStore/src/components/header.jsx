4db78ccebe993144d765e66a00f6903ddf1afaad
Ayuuu0424 <payushi9877@gmail.com>
2026-06-12 23:39:12 +0530

    Refactor code structure for improved readability and maintainability

--- FrontEnd/fakeStore/src/components/header.jsx
+++ FrontEnd/fakeStore/src/components/header.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaStore } from "react-icons/fa";
 
-const header = () => {
+const Header = () => {
   return (
     <>
       <div className="bg-blue-500 flex justify-between p-3">
-        <div>
-          <FaStore className="text-2xl text-white"/>
+        <div className="flex gap-3">
+          <FaStore className="text-2xl text-white" />
         </div>
+        <span>Fake Store</span>
         <div className="flex gap-4">
-          <Link to={"/"} className="text-white hover:underline">Home</Link>
-          <Link to={"/about"} className="text-white hover:underline">About</Link>
-          <Link to={"/contact"} className="text-white hover:underline">Contact</Link>
-          <Link to={"/products"} className="text-white hover:underline">Products</Link>
+          <Link to={"/"} className="text-white hover:underline">
+            Home
+          </Link>
+          <Link to={"/about"} className="text-white hover:underline">
+            About
+          </Link>
+          <Link to={"/contact"} className="text-white hover:underline">
+            Contact
+          </Link>
+          <Link to={"/products"} className="text-white hover:underline">
+            Products
+          </Link>
         </div>
       </div>
     </>
   );
 };
 
-export default header;
+export default Header;
--- FrontEnd/fakeStore/src/pages/Products.jsx
+++ FrontEnd/fakeStore/src/pages/Products.jsx
@@ -1,94 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import loading from "../assets/loading.gif";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchProducts = async () => {
+    try {
+      setIsLoading(true);
+
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      setIsError(true);
+      setErrorMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="p-10">
-      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <div className="border rounded-lg shadow-md p-4 flex flex-col justify-between w-75 h-100">
-          <div className="h-48 flex items-center justify-center">
-            <img
-              src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-              alt="Product"
-              className="h-full object-contain"
-            />
-          </div>
-
-          <h3 className="mt-4 text-center font-medium text-gray-800">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-            quo.
-          </h3>
-
-          <button className="mt-4 bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600">
-            Add To Cart
-          </button>
+    <>
+      {isLoading ? (
+        <div className="w-full h-[90vh] flex justify-center items-center">
+          <img src={loading} alt="" className="w-50" />
         </div>
-      </div>
+      ) : (
+        <div className="p-10 grid grid-cols-4 gap-5">
+          {products.length > 0 &&
+            products.map((product, index) => (
+              <div className="w-75 border rounded h-100 p-3" key={index}>
+                <div className="w-full h-40">
+                  <img
+                    src={product.image}
+                    alt=""
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+                <div className="flex flex-col justify-between h-50">
+                  <div>
+                    <p className="text-xl" title={product.title}>
+                      {product.title.length > 50
+                        ? product.title.slice(0, 47) + "..."
+                        : product.title}
+                    </p>
+                    <p className="text-sm capitalize text-gray-500">
+                      {product.category}
+                    </p>
+                    <p>₹ {product.price * 100}</p>
+                    <p>{product.rating.rate}/5</p>
+                  </div>
 
-      <div className="w-full h-50 border rounded-lg shadow-md p-4 mt-8 flex items-center gap-4 ">
-        <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-          alt=""
-          className="h-full object-contain"
-        />
-
-        <div>
-          <h2 className="text-xl font-semibold">Product Name</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  <button className="bg-orange-400 rounded-full px-4 py-2">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))}
         </div>
-
-        <div>
-          <button className="mt-4 bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600">
-            Add To Cart
-          </button>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
 export default Products;
-
-// import React from "react";
-
-// const Products = () => {
-//   return (
-//     <div className="p-10">
-//       {/* Vertical Card */}
-//       <div className="grid grid-cols-4 gap-6">
-//         <div className="border rounded-lg shadow-md p-4">
-//           <div className="h-48 flex items-center justify-center">
-//             <img
-//               src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-//               alt=""
-//               className="h-full object-contain"
-//             />
-//           </div>
-
-//           <p className="mt-4">
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//           </p>
-
-//           <button className="mt-4 bg-pink-500 text-white px-4 py-2 rounded">
-//             Add To Cart
-//           </button>
-//         </div>
-//       </div>
-
-//       {/* Horizontal Card */}
-//       <div className="w-full h-52 border rounded-lg shadow-md p-4 mt-8 flex items-center gap-4">
-//         <img
-//           src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-//           alt=""
-//           className="h-full object-contain"
-//         />
-
-//         <div>
-//           <h2 className="text-xl font-semibold">Product Title</h2>
-//           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Products;
