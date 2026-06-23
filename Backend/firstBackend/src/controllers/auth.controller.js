b39ff52d0a3f30e7f5a4d2d30c8b1ad845945bc0
Ayuuu0424 <payushi9877@gmail.com>
2026-06-23 21:08:00 +0530

    feat: implement user registration and mongoose model, add MongoDB connection

--- Backend/firstBackend/src/controllers/auth.controller.js
+++ Backend/firstBackend/src/controllers/auth.controller.js
@@ -1,9 +1,26 @@
-export const LoginUser = (req, res) => {
-  res.json({ message: "Login Successful from Controller" });
+import User from "../models/user.model.js";
+
+export const RegisterUser = async (req, res) => {
+  try {
+    const { fullName, email, password, phone, gender, dob } = req.body;
+
+    if (!fullName || !email || !password || !phone || !gender || !dob) {
+      res.status(400).json({ message: "All fields required" });
+      return;
+    }
+
+    const existingUSer = await User.findOne({ email });
+    if (existingUSer) {
+      res.status(409).json({ message: "Email already registered" });
+      return;
+    }
+
+    //Create New User and Complete registration => tomorrow
+  } catch (errror) {}
 };
 
-export const RegisterUser = (req, res) => {
-  res.json({ message: "Registration Successful from Controller" });
+export const LoginUser = (req, res) => {
+  res.json({ message: "Login Successful from Controller" });
 };
 
 export const LogoutUser = (req, res) => {
